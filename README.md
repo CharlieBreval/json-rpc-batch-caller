f24b10ff240967ebce17e4f644691b0fc9dfc344
CharlieBreval <cbreval@lafourchette.com>
2020-06-04 10:49:26 +0200

    Fix

--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 
 Just configure the calls you want to do by creating files in the folder './calls'
 Then run 'node index.js'
-
--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
+const config = require("config");
 
-const delayTime = 3000;
+const delayTime = config.get("delay");
 
-handle('./calls');
+handle("./calls");
 
 async function handle(dirName) {
-  await fs.readdir(dirName, ((err, items) => {
+  await fs.readdir(dirName, (err, items) => {
     items.forEach(async (fileName) => {
-      const fileContent = fs.readFileSync(dirName + '/' + fileName);
-      
-      const paramsList = JSON.parse(fileContent)['params'];
-      const method = JSON.parse(fileContent)['method'];
-      const host = JSON.parse(fileContent)['host'];
-      const path = JSON.parse(fileContent)['path'];
+      const fileContent = fs.readFileSync(dirName + "/" + fileName);
+
+      const paramsList = JSON.parse(fileContent)["params"];
+      const method = JSON.parse(fileContent)["method"];
+      const host = config.get("host");
+      const path = config.get("path");
 
       for (const params of paramsList) {
         const result = await call(host, path, method, params);
@@ -21,55 +22,56 @@ async function handle(dirName) {
         await delay();
       }
     });
-  }));
+  });
 }
 
 function call(host, path, method, params) {
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     const httpOptions = {
       hostname: host,
-      port: '80',
+      port: "80",
       path,
-      method: 'POST',
-      headers: {"Content-Type":"application/json; charset=utf-8"}
+      method: "POST",
+      headers: { "Content-Type": "application/json; charset=utf-8" },
     };
 
-      var req = http.request(httpOptions, function(res) {
-        if (res.statusCode < 200 || res.statusCode >= 300) {
-          return reject(new Error('statusCode=' + res.statusCode));
+    var req = http.request(httpOptions, function (res) {
+      if (res.statusCode < 200 || res.statusCode >= 300) {
+        return reject(new Error("statusCode=" + res.statusCode));
+      }
+
+      var body = [];
+      res.on("data", function (chunk) {
+        body.push(chunk);
+      });
+
+      res.on("end", function () {
+        try {
+          body = JSON.parse(Buffer.concat(body).toString());
+        } catch (e) {
+          reject(e);
         }
-
-        var body = [];
-        res.on('data', function(chunk) {
-          body.push(chunk);
-        });
-
-        res.on('end', function() {
-          try {
-              body = JSON.parse(Buffer.concat(body).toString());
-          } catch(e) {
-              reject(e);
-          }
-          resolve(body);
-        });
+        resolve(body);
       });
+    });
 
-      
-      req.on('error', function(err) {
-          reject(err);
-      });
-      
-      req.write(JSON.stringify({
-        'jsonrpc': '2.0',
-        'id': 'ID321',
-        'method': method,
-        'params': params 
-      }));
+    req.on("error", function (err) {
+      reject(err);
+    });
 
-      req.end();
+    req.write(
+      JSON.stringify({
+        jsonrpc: "2.0",
+        id: "ID321",
+        method: method,
+        params: params,
+      })
+    );
+
+    req.end();
   });
 }
 
 function delay() {
-  return new Promise(resolve => setTimeout(resolve, delayTime));
+  return new Promise((resolve) => setTimeout(resolve, delayTime));
 }
